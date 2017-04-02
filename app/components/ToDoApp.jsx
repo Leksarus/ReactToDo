eb6ff08bdc695630a0566def3579c8133b2e46df
Leksarus <kamil.nawrocki5@gmail.com>
2017-04-02 14:29:58 +0200

    Change app to work with local storage.

--- app/components/ToDoApp.jsx
+++ app/components/ToDoApp.jsx
@@ -5,52 +5,18 @@ var moment = require('moment');
 import ToDoList from 'ToDoList';
 import AddTodo from 'AddTodo';
 import TodoSearch from 'TodoSearch';
-var TodoAPI = require('TodoAPI');
 
 var ToDoApp = React.createClass({
-	getInitialState: function() {
-		return {
-			showCompleted: false,
-			searchText: '',
-			todos: TodoAPI.getTodos()
-		};
-	},
-	componentDidUpdate: function() {
-		TodoAPI.setTodos(this.state.todos);
-	},
-	handleAddTodo: function (text) {
-		this.setState({
-			todos: [
-				...this.state.todos,
-				{
-					id: uuid(),
-					text: text,
-					completed: false,
-					createdAt: moment().unix(),
-					completedAt: undefined
-				}
-			]
-		});
-	},
-	handleSearch: function (showCompleted, searchText) {
-		this.setState({
-			showCompleted: showCompleted,
-			searchText: searchText.toLowerCase()
-		});
-	},
 	render: function() {
-		var { todos, showCompleted, searchText } = this.state;
-		var filteredTodos = TodoAPI.filterTodos(todos, showCompleted, searchText);
-
 		return (
 			<div>
 				<h1 className="page-title">Taskozarządzacz</h1>
 				<div className="row">
 					<div className="column small-centered small-11 medium-6 large-5">
 						<div className="container">
-							<TodoSearch onSearch={this.handleSearch}/>
+							<TodoSearch/>
 							<ToDoList />
-							<AddTodo handleAddTodo={this.handleAddTodo} />
+							<AddTodo/>
 							<p><span>Wykonanie:</span><span>Kamil Nawrocki</span></p>
 						</div>
 					</div>
--- app/reducers/reducers.jsx
+++ app/reducers/reducers.jsx
@@ -46,6 +46,11 @@ export var todosReducer = (state = [], action) => {
 					return todo;
 				}
 			}); 
+		case 'ADD_TODOS':
+			return [
+				...state,
+				...action.todos
+			];	
 		default:
 			return state;
 		// add case for TOGGLE_TODO completed equal to opposite value & updateCompletedAt		
